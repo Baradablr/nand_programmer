5774ef666753c6a2e69fba487edcee721c03e332
Andrei Paulau <7134956@gmail.com>
2022-06-17 14:49:40 +0300

    Add progress for read bad blocks operation.

--- qt/programmer.cpp
+++ qt/programmer.cpp
@@ -273,7 +273,14 @@ void Programmer::readChipBadBlocksCb(int ret)
     serialPort.stop();
     QObject::disconnect(&reader, SIGNAL(result(int)), this,
         SLOT(readChipBadBlocksCb(int)));
+    QObject::disconnect(&reader, SIGNAL(progress(unsigned int)), this,
+        SLOT(readChipBadBlocksProgressCb(unsigned int)));
     emit readChipBadBlocksCompleted(ret);
+}
+
+void Programmer::readChipBadBlocksProgressCb(unsigned int progress)
+{
+    emit readChipBadBlocksProgress(progress);
 }
 
 void Programmer::readChipBadBlocks()
@@ -282,6 +289,8 @@ void Programmer::readChipBadBlocks()
 
     QObject::connect(&reader, SIGNAL(result(int)), this,
         SLOT(readChipBadBlocksCb(int)));
+    QObject::connect(&reader, SIGNAL(progress(unsigned int)), this,
+        SLOT(readChipBadBlocksProgressCb(unsigned int)));
 
     cmd.code = CMD_NAND_READ_BB;
 
